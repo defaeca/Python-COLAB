f2aaa536f702f349cdac8429f0d9ee5168b9570d
Raphael Eça <raphael2904@gmail.com>
2024-03-23 12:28:55 -0300

    COLAB

---
+++ 1_50/2_SolicitacaoDeNumero.py
@@ -0,0 +1,6 @@
+# Faça um programa que solicite um número do usuário e apresente a seguinte mensagem na tela:
+# "O número digitado foi [número]".
+
+
+numero = float(input("Digite um numero: "))
+print(f"o numero digitado foi {numero}")
---
+++ 1_50/4_NumerosInteiros.py
@@ -0,0 +1,9 @@
+# Faça um programa que solicite três números inteiros do usuário e imprima a soma destes.
+print("SOMA DOS NUMEROS INTEIROS")
+
+num1 = int(input("Numero 1:"))
+num2 = int(input("Numero 2:"))
+num3 = int(input("Numero 3:"))
+soma = num1 + num2 + num3
+
+print(f"Resultado:", soma)
